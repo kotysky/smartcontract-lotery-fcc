6f1dfb7b176d8fc51d6bf45fa8a9c4ebc52b6caa
JJ Huertas <kotysky@Belcebu>
2023-10-16 08:31:22 +0100

    inicial

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-deploy")
+require("dotenv").config()
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.19",
+}
