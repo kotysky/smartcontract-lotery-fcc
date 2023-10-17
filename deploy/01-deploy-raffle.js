9a41fef6d7761735575bab20be23ed4c03d8a37b
kotysky <kotysky@DESKTOP-IK71H8Q>
2023-10-17 17:02:22 +0100

    17/10 2

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,13 +1,55 @@
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../helper-hardhat-config")
+const VRF_SUB_FUND_AMOUNT = ethers.parseEther("2")
 
-module.exports = async ({ getNamedAccounts, deployment }) => {
-    const { deploy, log } = deployment
-    const { deployer } = await getNamedAccounts
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    let vrfCoordinatorV2address, subscriptionId
 
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract(
+            //// COSAS RARAS
+            "VRFCoordinatorV2Mock",
+        )
+        vrfCoordinatorV2address = vrfCoordinatorV2Mock.address
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const transactionRecipt = await transactionResponse.wait(1)
+        subscriptionId = transactionRecipt.event[0].args.subId
+        //We have the subscription, then we need to fund the suscription
+        //Usually you need link token  to a real network
+        await vrfCoordinatorV2Mock.fundSubcription(subscriptionId, VRF_SUB_FUND_AMOUNT)
+    } else {
+        vrfCoordinatorV2address = netwotkConfig[chainId]["vrfCoodinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
+    }
+    const entranceFee = networkConfig[chainId]["entranceFee"]
+    const gasLane = networkConfig[chanId]["gasLane"]
+    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+    const interval = networkConfig[chainId][interval]
+
+    const args = [
+        vrfCoordinatorV2address,
+        entranceFee,
+        gasLane,
+        subscriptionId,
+        callbackGasLimit,
+        interval,
+    ]
     const raffle = await deploy("Raffle", {
         from: deployer,
-        args: [],
+        args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying!...")
+        await verify(raffle.address, args)
+    }
+    log("---------------------------------------------------------")
 }
+
+module.exports.tags = ["all", "raffle"]
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,11 +1,25 @@
+const { ethers } = require("hardhat")
+
 const networkConfig = {
     1115111: {
         name: "sepolia",
         vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+        entranceFee: ethers.parseEther("0.01"),
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        subcriptionId: "0",
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+    31337: {
+        name: "hardhat",
+        entranceFee: ethers.parseEther("0.01"),
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        callbackGasLimit: "500000",
+        interval: "30",
     },
 }
 const developmentChains = ["hardhat", "localhost"]
 module.exports = {
-    netwotkConfig,
+    networkConfig,
     developmentChains,
 }
