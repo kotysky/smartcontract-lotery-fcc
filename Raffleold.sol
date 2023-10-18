cb4d08c124bfdac0052014eb571a3721430e521d
kotysky <kotysky@DESKTOP-IK71H8Q>
2023-10-18 13:32:08 +0100

    funcionando

---
+++ Raffleold.sol
@@ -0,0 +1,174 @@
+// Raffle
+// Enter ther lottery (paying some amount)
+//Pick a random winner(verifable random)
+// Winner to be selected every X minutes -> completly automated
+// Chainlink Oracle -> Randomness, Automated Excution(Chainlink keerpers)
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+
+error Raffle__NotEnoughETHEnterd();
+error Raffle__TransferFailed();
+error Raffle__NotOpen();
+error Raffle__UpkeepNotNeeded(uint256 currentBalance, uint256 numPlayers, uint256 raffleState);
+
+/**
+ * @title Raffle contract
+ * @author JJ Huertas
+ * @notice This contract implements a untemporary lotery
+ * @dev This implements Chainlink VRF V2 and ChainLink keepers
+ */
+
+contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
+    /* Type declarations */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    } //uint256 0=OPEN, 1=CALCULATING
+
+    /* State Variables */
+    uint256 private immutable i_entranceFee; // the free to take a possibility to win
+    address payable[] private s_players; // the list of players of the the Raffle
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator; // This is the addres of the contract to make the random
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    // Lottery variables
+    address private s_recentWinner;
+    RaffleState private s_raffleState;
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
+
+    /* Events */
+    event RaffleEnter(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed winner);
+
+    /* functions */
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit,
+        uint256 interval
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_raffleState = RaffleState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        i_interval = interval;
+    }
+
+    function enterRaffle() public payable {
+        // require (msg.value > i_enranceFee,"not enough ETH!")
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEnterd();
+        }
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__NotOpen();
+        }
+        s_players.push(payable(msg.sender)); // insert de new sender
+
+        // Events when we update a dynamic object
+        emit RaffleEnter(msg.sender);
+    }
+
+    /**
+     * @dev This is the function that chainlink keeper nodes call
+     * They look for the 'upkeepNeeded' return true
+     * The following shold be true in order to return true
+     */
+
+    function checkUpKeep(
+        bytes memory /*checkData*/
+    ) public returns (bool upKeepNeeded, bytes memory /* performData */) {
+        bool isOpen = (RaffleState.OPEN == s_raffleState);
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        bool hasPlayers = (s_players.length > 0);
+        bool hasBalance = address(this).balance > 0;
+        upKeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
+    }
+
+    function performUpKeep(bytes calldata /*performData*/) external {
+        (bool upKeepNeeded, ) = checkUpKeep("");
+
+        if (!upKeepNeeded) {
+            revert Raffle__UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_raffleState)
+            );
+        }
+
+        s_raffleState = RaffleState.CALCULATING;
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, // Gaslane
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedRaffleWinner(requestId);
+    }
+
+    function fulfillRandomWords(
+        uint256 /*requestId*/,
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
+    }
+
+    /* View / Pure Functions */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayers(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
+    }
+
+    function getRaffleState() public view returns (RaffleState) {
+        return s_raffleState;
+    }
+
+    function getNumWords() public pure returns (uint256) {
+        return NUM_WORDS;
+    }
+
+    function getNumberOfPlayers() public view returns (uint256) {
+        return s_players.length;
+    }
+
+    function getLastTimeStamp() public view returns (uint256) {
+        return s_lastTimeStamp;
+    }
+
+    function getRequestConfirmations() public pure returns (uint256) {
+        return REQUEST_CONFIRMATIONS;
+    }
+}
--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -8,48 +8,25 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
     let vrfCoordinatorV2address, subscriptionId, vrfCoordinatorV2Mock
-    /////////
-    /*const accounts = await ethers.getSigners()
-    signer = accounts[0]*/
-    /////////
 
     if (developmentChains.includes(network.name)) {
-        /*vrfCoordinatorV2Mock = await ethers.getContract(
-            //// COSAS RARAS
-            "VRFCoordinatorV2Mock",
-        )
-        vrfCoordinatorV2address = vrfCoordinatorV2Mock.address*/
-        //////////////////////////////////////////////
         const vrfCoordinatorV2MockDeploy = await deployments.get("VRFCoordinatorV2Mock") //Guauu!! parece que funciona esto
-        //vrfCoordinatorV2address = vrfCoordinatorV2MockDeploy.address
-        //vrfCoordinatorV2address = vrfCoordinatorV2Mock.address
-        //log(vrfCoordinatorV2MockDeploy)
-        /////////////////////////////////////////////
+        vrfCoordinatorV2address = vrfCoordinatorV2MockDeploy.address /// Tu puta madreee!!!
+
         vrfCoordinatorV2Mock = await ethers.getContractAt(
             vrfCoordinatorV2MockDeploy.abi,
             vrfCoordinatorV2MockDeploy.address,
-            //signer,
         )
-        //vrfCoordinatorV2address = MockV2Coordinator.address
-        /////////////////////////////////////////////
 
         const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
-        //const transactionResponse = await vrfCoordinatorV2MockDeploy.createSubscription()
-        //log(transactionResponse)
         const transactionReceipt = await transactionResponse.wait(1)
-        //log(transactionReceipt)
 
-        //subscriptionId = transactionReceipt.events[0].args.requestId
-        /*subscriptionId = transactionReceipt.logs[1].args.requestId
-         */
+        //subscriptionId = transactionReceipt.events[0].args.subId
+        //subscriptionId = transactionReceipt.logs[1].args.requestId
+
         subscriptionId = 1
-        log(subscriptionId)
-
-        //We have the subscription, then we need to fund the suscription
-        //Usually you need link token  to a real network
 
         await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
-        //await vrfCoordinatorV2MockDeploy.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
     } else {
         vrfCoordinatorV2address = networkConfig[chainId]["vrfCoodinatorV2"]
         subscriptionId = networkConfig[chainId]["subscriptionId"]
@@ -60,7 +37,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
     const interval = networkConfig[chainId]["interval"]
 
-    const args = [
+    const arguments = [
         vrfCoordinatorV2address,
         entranceFee,
         gasLane,
@@ -71,8 +48,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
     const raffle = await deploy("Raffle", {
         from: deployer,
-        //from: signer,
-        args: args,
+        args: arguments,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
